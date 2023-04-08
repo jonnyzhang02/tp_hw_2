686531f4735d8f0ed05e5b8e027898648c36dc79
jonnyzhang02 <71881972+jonnyzhang02@users.noreply.github.com>
2023-04-08 20:22:23 +0800

    FINISHED

---
+++ BiLSTM-CRF.py
@@ -0,0 +1,207 @@
+import json
+
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long)
+
+def argmax(vec):
+	# 得到最大的值的索引
+	_, idx = torch.max(vec, 1)
+	return idx.item()
+
+def log_sum_exp(vec):
+	max_score = vec[0, argmax(vec)]  # max_score的维度为1
+	max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])  # 维度为1*5
+	return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+	#等同于torch.log(torch.sum(torch.exp(vec)))，防止e的指数导致计算机上溢
+
+class BiLSTM_CRF(nn.Module):
+	def __init__(self, vocab_size, tag_to_ix, embedding_dim, hidden_dim):
+		super(BiLSTM_CRF, self).__init__()
+		self.embedding_dim = embedding_dim
+		self.hidden_dim = hidden_dim
+		self.vocab_size = vocab_size
+		self.tag_to_ix = tag_to_ix
+		self.tagset_size = len(tag_to_ix)
+
+		self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
+		self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, num_layers=1, bidirectional=True)
+		self.hidden2tag = nn.Linear(hidden_dim, self.tagset_size)
+		# 转移矩阵，transitions[i][j]表示从label_j转移到label_i的概率,虽然是随机生成的但是后面会迭代更新
+		self.transitions = nn.Parameter(torch.randn(self.tagset_size, self.tagset_size))
+
+		self.transitions.data[tag_to_ix[START_TAG], :] = -10000  # 从任何标签转移到START_TAG不可能
+		self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000  # 从STOP_TAG转移到任何标签不可能
+
+		self.hidden = self.init_hidden() # 随机初始化LSTM的输入(h_0, c_0)
+
+	def init_hidden(self):
+		return (torch.randn(2, 1, self.hidden_dim // 2),
+				torch.randn(2, 1, self.hidden_dim // 2))
+
+	def _forward_alg(self, feats):
+		'''
+		输入：发射矩阵(emission score)，实际上就是LSTM的输出——sentence的每个word经BiLSTM后，对应于每个label的得分
+		输出：所有可能路径得分之和/归一化因子/配分函数/Z(x)
+		'''
+		init_alphas = torch.full((1, self.tagset_size), -10000.)
+		init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+
+		# 包装到一个变量里面以便自动反向传播
+		forward_var = init_alphas
+		for feat in feats: # w_i
+			alphas_t = []
+			for next_tag in range(self.tagset_size): # tag_j
+				# t时刻tag_i emission score（1个）的广播。需要将其与t-1时刻的5个previous_tags转移到该tag_i的transition scors相加
+				emit_score = feat[next_tag].view(1, -1).expand(1, self.tagset_size) # 1*5
+				# t-1时刻的5个previous_tags到该tag_i的transition scors
+				trans_score = self.transitions[next_tag].view(1, -1)  # 维度是1*5
+				next_tag_var = forward_var + trans_score + emit_score
+				# 求和，实现w_(t-1)到w_t的推进
+				alphas_t.append(log_sum_exp(next_tag_var).view(1))
+			forward_var = torch.cat(alphas_t).view(1, -1) # 1*5
+
+		# 最后将最后一个单词的forward var与转移 stop tag的概率相加
+		terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+		alpha = log_sum_exp(terminal_var)
+		return alpha
+
+	def _get_lstm_features(self, sentence):
+		'''
+		输入：id化的自然语言序列
+		输出：序列中每个字符的Emission Score
+		'''
+		self.hidden = self.init_hidden() # (h_0, c_0)
+		embeds = self.word_embeds(sentence).view(len(sentence), 1, -1)
+		lstm_out, self.hidden = self.lstm(embeds, self.hidden)
+		lstm_out = lstm_out.view(len(sentence), self.hidden_dim)
+		lstm_feats = self.hidden2tag(lstm_out) # len(s)*5
+		return lstm_feats
+
+	def _score_sentence(self, feats, tags):
+		'''
+		输入：feats——emission scores；tags——真实序列标注，以此确定转移矩阵中选择哪条路径
+		输出：真实路径得分
+		'''
+		score = torch.zeros(1)
+		# 将START_TAG的标签３拼接到tag序列最前面
+		tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+		for i, feat in enumerate(feats):
+			score = score + \
+					self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
+		score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+		return score
+
+	def _viterbi_decode(self, feats):
+		# 预测序列的得分，维特比解码，输出得分与路径值
+		backpointers = []
+
+		init_vvars = torch.full((1, self.tagset_size), -10000.)
+		init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+
+		forward_var = init_vvars
+		for feat in feats:
+			bptrs_t = []
+			viterbivars_t = []
+
+			for next_tag in range(self.tagset_size):
+				next_tag_var = forward_var + self.transitions[next_tag]  # forward_var保存的是之前的最优路径的值
+				best_tag_id = argmax(next_tag_var)  # 返回最大值对应的那个tag
+				bptrs_t.append(best_tag_id)
+				viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+
+			forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+			backpointers.append(bptrs_t)  # bptrs_t有５个元素
+
+		# 其他标签到STOP_TAG的转移概率
+		terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+		best_tag_id = argmax(terminal_var)
+		path_score = terminal_var[0][best_tag_id]
+
+		best_path = [best_tag_id]
+		for bptrs_t in reversed(backpointers):
+			best_tag_id = bptrs_t[best_tag_id]
+			best_path.append(best_tag_id)
+		
+		# 无需返回最开始的START位
+		start = best_path.pop()
+		assert start == self.tag_to_ix[START_TAG]
+		best_path.reverse()  # 把从后向前的路径正过来
+		return path_score, best_path
+
+	def neg_log_likelihood(self, sentence, tags):  # 损失函数
+		feats = self._get_lstm_features(sentence)  # len(s)*5
+		forward_score = self._forward_alg(feats)  # 规范化因子/配分函数
+		gold_score = self._score_sentence(feats, tags) # 正确路径得分
+		return forward_score - gold_score  # Loss（已取反）
+
+	def forward(self, sentence):
+		'''
+		解码过程，维特比解码选择最大概率的标注路径
+		'''
+		lstm_feats = self._get_lstm_features(sentence)
+
+		score, tag_seq = self._viterbi_decode(lstm_feats)
+		return score, tag_seq
+
+START_TAG = "<START>"
+STOP_TAG = "<STOP>"
+EMBEDDING_DIM = 5 # 由于标签一共有B\I\O\START\STOP 5个，所以embedding_dim为5
+HIDDEN_DIM = 4 # 这其实是BiLSTM的隐藏层的特征数量，因为是双向所以是2倍，单向为2
+
+data = json.load(open('./data/data.json', 'r', encoding='utf-8'))
+print(data[1])
+
+# training_data = [(
+# 	"the wall street journal reported today that apple corporation made money".split(),
+# 	"B I I I O O O B I O O".split()
+# ), (
+# 	"georgia tech is a university in georgia".split(),
+# 	"B I O O O O B".split()
+# )]
+
+word_to_ix = {}
+for piece in data:
+	for word in piece["text"]:
+		if word not in word_to_ix:
+			word_to_ix[word] = len(word_to_ix)
+
+tag_to_ix = {"O": 0, "B-LOCATION": 1, "I-LOCATION": 2, "B-TIME": 3, "I-TIME": 4, START_TAG: 5, STOP_TAG: 6}
+
+model = BiLSTM_CRF(len(word_to_ix), tag_to_ix, EMBEDDING_DIM, HIDDEN_DIM)
+optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
+print(model)
+
+i = 0
+for epoch in range(1):
+	for piece in data:
+		sentence = piece["text"]
+		tags = piece["label"]
+		model.zero_grad()
+		
+        # 输入
+		sentence_in = prepare_sequence(sentence, word_to_ix)
+		targets = torch.tensor([tag_to_ix[t] for t in tags], dtype=torch.long)
+		
+        # 获取loss
+		loss = model.neg_log_likelihood(sentence_in, targets)
+		print(i/len(data), "\tloss:", loss.item())
+		i += 1
+        # 反向传播
+		loss.backward()
+		optimizer.step()
+
+model.eval()
+# 保存模型
+torch.save(model, './model.pkl')
+
+with torch.no_grad():
+	precheck_sent = prepare_sequence(data[0]["text"], word_to_ix)
+	print(model(precheck_sent))
---
+++ dispose_data.py
@@ -0,0 +1,161 @@
+import os
+import re
+import json
+import jieba
+
+
+
+def transfer_data(): 
+    data = []
+    """
+    转换地址数据
+    """    
+    piece = {}
+    files = os.listdir("./data/location")  # 得到文件夹下的所有文件名称
+    for file in files: # 遍历文件夹
+        with open("./data/location/"+file, 'r', encoding='utf-8') as f: # 打开文件
+            text = f.read()
+            text = re.sub("\n", "", text)
+            piece["label"] = list(range(len(text)))  # 标签、文本长度一致
+
+            # 先将所有标签置为O
+            for i in range(len(text)): #
+                piece["label"][i] = "O"
+
+            # 进行标签转换
+            human_labels_index = [] # 人工标注的标签索引
+            for m in re.finditer(r'\s([^\s]*)/LOC', text): # 1.匹配空格 2.匹配非空格内容 3.匹配/LOC
+                piece["label"][m.start()+1] = "B-LOCATION" # 地址开始的标签为B-LOCATION
+                for i in range(m.start()+2, m.end()-4): # 地址中间的标签为I-LOCATION
+                    piece["label"][i] = "I-LOCATION"
+                for i in range(m.end()-4, m.end()): # 人工标注的标签索引
+                    human_labels_index.append(i)
+
+            # 去除人工标注的标签
+            piece["text"] = list(text) # 文本
+            for i in human_labels_index: 
+                piece["text"][i] = " " # 将人工标注的标签置为空格
+            data.append(piece) # 添加到数据中
+            piece = {} 
+            
+    """
+    转换时间数据
+    """
+    files = os.listdir("./data/time")  
+    for file in files:  # 遍历文件夹
+        with open("./data/time/"+file, 'r', encoding='utf-8') as f: # 打开文件
+            text = f.read() # 读取文件
+            text = re.sub("\n", "", text) # 去除换行符
+            piece["label"] = list(range(len(text))) # 标签、文本长度一致
+            human_labels_index = [] # 人工标注的标签索引
+
+            # 先将所有标签置为O
+            for i in range(len(text)): 
+                piece["label"][i] = "O"         # 其他标签为O
+
+            # 进行标签转换
+            for m in re.finditer(r'\s([^\s]*)/[DT][SO]', text): #1.匹配空格 2.匹配非空格内容 3.匹配/LOC
+                piece["label"][m.start()+1] = "B-TIME"       # 时间开始的标签为B-TIME
+                for i in range(m.start()+2, m.end()-3):
+                    piece["label"][i] = "I-TIME"            # 时间中间的标签为I-TIME
+                for i in range(m.end()-3, m.end()):
+                    human_labels_index.append(i)           # 人工标注的标签索引
+
+            # 去除人工标注的标签
+            piece["text"] = list(text) # 文本
+            for i in human_labels_index: #
+                piece["text"][i] = " " # 将人工标注的标签置为空格
+            
+            data.append(piece) # 添加到数据中
+            piece = {}
+    
+    # 保存数据为json格式
+    # json.dump(data, open("./data/data.json", 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+
+    # 保存数据为txt格式
+    with open("./data/data.txt", 'w', encoding='utf-8') as f:
+        for piece in data:
+            for i in range(len(piece["text"])):
+                f.write(piece["text"][i] + " " + piece["label"][i] + "\n")
+            f.write("\n")
+
+    return data
+
+def add_line_sp():
+    # 添加分隔符(每个30行加一次)
+    f = open('./data/data.txt','r',encoding='utf-8')
+    f2 = open('./data/total_ner2.txt','w',encoding='utf-8')
+    tag_list = ['。','！','？','；']
+    for line in f:
+        f2.write(line)
+        line_sps = line.strip().split(' ')
+        if line_sps[0] in tag_list:
+            line_sp = line.strip().split(' ')
+            if len(line_sp) == 2:
+                if line_sp[1] == 'O':
+                    f2.write('\n')
+                    line_num = 0
+            else:
+                    f2.write('\n')
+                    line_num = 0
+    print('成功添加分隔符！')
+
+def spilt_data():
+    print('\n')
+    print('开始切割ner的total_ner2.txt数据：')
+    # f_path = 'ner_data/2/test' + str(k) + '.txt'
+    f_path = './data/total_ner2.txt'
+    train_path = './split_data/train.txt'
+    valid_path = './split_data/dev.txt'
+    test_path = './split_data/test.txt'
+    f = open(f_path,'r',encoding='utf-8')
+    f1 = open(f_path, 'r', encoding='utf-8')
+    f_train = open(train_path,'w',encoding='utf-8')
+    f_valid = open(valid_path, 'w', encoding='utf-8')
+    f_test = open(test_path, 'w', encoding='utf-8')
+    line_num = 0
+    for line in f:
+        line_num += 1
+    print(line_num)
+    train_num = int(line_num * 0.7)
+    # train_num = int(line_num * 0.8)  # 训练集为8：1：1时使用该句
+    print(train_num)
+    valid_num = int(line_num * 0.1)
+    print(valid_num)
+    # test_num =
+    count_num = 0
+    count_1 = 0
+    for line_train in f1:
+        count_num += 1
+        if count_num <= train_num :
+            # count_1 +=1
+            f_train.write(line_train)
+        else:
+            line_sp = line_train.strip().split(' ')
+            if len(line_sp) == 3:
+                    f_train.write(line_train)
+            else:
+                break
+    for line_valid in f1:
+        count_1 += 1
+        if count_1 <= valid_num :
+            # count_1 +=1
+            f_valid.write(line_valid)
+        else:
+            line_sp = line_valid.strip().split(' ')
+            if len(line_sp) == 3:
+                f_valid.write(line_valid)
+            else:
+                break
+    for line_test in f1:
+        f_test.write(line_test)
+    print('切割完成！')
+
+def modify_data():
+    
+    transfer_data()       
+
+    add_line_sp() 
+
+    spilt_data()
+    
